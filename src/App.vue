4280151adb845873efaa5fce6ddb15111b2e647d
David Hoang <david.hoang@hotmail.fr>
2026-04-08 16:07:49 +0200

    refactor: max width select

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,14 @@
       </div>
       <div class="p-5 flex flex-col gap-1">
         <div class="flex justify-between">
-          <select name="targets" id="users" class="border border-zinc-700 bg-state-200 px-2 py-1 rounded-md">
-            <option value="targets1">User 1</option>
-            <option value="targets2">User 2</option>
-            <option value="targets3">User 3</option>
+          <select name="targets" id="users" class="border border-zinc-700 bg-state-200 px-2 py-1 rounded-md max-w-28">
+            <option value="target1">User 1</option>
+            <option value="target2">User 2</option>
+            <option value="target3">User 3</option>
+            <option value="target4">Jean-Michel ABCDEFGHIJKLMOP</option>
           </select>
 
-          <button class="rounded-md bg-red-400 px-2 py-1 text-white">Déconnexion</button>
+          <button class="rounded-md bg-red-400 px-2 py-1 text-white cursor-pointer">Déconnexion</button>
         </div>
         <input v-model="message" class="w-full rounded-md border border-zinc-700 bg-state-200 p-2" placeholder="Votre messages...">
         </input>
