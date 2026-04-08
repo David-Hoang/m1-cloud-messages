e4c3e83008876cf35a37d588575d63a826217e41
David Hoang <david.hoang@hotmail.fr>
2026-04-08 15:50:08 +0200

    add front input message

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const message = ref('');
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <p class="text-lg text-gray-600 bg-red-500 ">
-    This is a simple Vue.js app with Tailwind CSS styling.
-  </p>
+  <main class="p-5 bg-state-100 h-dvh flex justify-center items-center">
+    <div class="rounded-xl border border-zinc-700 shadow-xl h-2/3 w-2/3 flex flex-col overflow-auto">
+      <div class="p-5 flex-1">
+        Messages
+      </div>
+      <div class="p-5">
+        <select name="user" id="users">
+          <option value="user1">User 1</option>
+          <option value="user2">User 2</option>
+          <option value="user3">User 3</option>
+        </select>
+        <input v-model="message" class="w-full rounded-md border border-zinc-700 bg-state-200 p-2" placeholder="Votre messages...">
+        </input>
+      </div>
+    </div>
+  </main>
 </template>
-
-<style scoped></style>
