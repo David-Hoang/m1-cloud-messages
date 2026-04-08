13dfd939cc33fbc562074005ea3e0b2bc5983341
David Hoang <david.hoang@hotmail.fr>
2026-04-08 15:30:06 +0200

    install tailwind and downgrade vite

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p class="text-lg text-gray-600 bg-red-500 ">
+    This is a simple Vue.js app with Tailwind CSS styling.
+  </p>
 </template>
 
 <style scoped></style>
